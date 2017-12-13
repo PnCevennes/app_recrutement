2a39952017cb68d27cadb552236376815f79bcce
administrateur <administrateur@apps-intra.pnc.int>
2017-12-13 16:25:22 +0100

    fix envoi mail, ajout liste email demandeurs

--- modules/interventions/routes.py
+++ modules/interventions/routes.py
@@ -112,7 +112,7 @@ def create_intervention():
                 Une nouvelle demande d'intervention a été créée.
                 Vous pouvez vous connecter sur http://tizoutis.pnc.int/#/interventions?intervention=%s pour voir les détails de cette demande.
                 ''' % demande.id,
-                add_dests = demande.dmdr_contact_email)
+                add_dests = demande.dmdr_contact_email.split(','))
 
         return {'id': demande.id}
     except ValidationError as e:
@@ -142,7 +142,7 @@ def update_intervention(id_intervention):
                 La demande d'intervention n°%s a été modifiée.
                 Vous pouvez vous connecter sur http://tizoutis.pnc.int/#/interventions?intervention=%s pour voir les détails de cette demande.
                 ''' % (demande.id, demande.id),
-                add_dests = demande.dmdr_contact_email)
+                add_dests = demande.dmdr_contact_email.split(','))
 
         return {'id': demande.id}
     except ValidationError as e:
@@ -166,7 +166,7 @@ def delete_intervention(id_intervention):
             La demande d'intervention n°%s a été annulée.
             Vous pouvez vous connecter sur http://tizoutis.pnc.int/#/interventions/ pour voir la liste des demandes en cours.
             ''',
-            add_dests = demande.dmdr_contact_email)
+            add_dests = demande.dmdr_contact_email.split(','))
 
     return {'id': demande.id}
 
